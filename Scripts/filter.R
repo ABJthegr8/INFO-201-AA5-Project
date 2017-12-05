9ad84f761dd164ae9de7e2c086701f1b7d400f9b
Abhijay Kumar <abhijaykumar@yahoo.in>
2017-12-04 19:05:18 -0800

    add comparison data for edu and employ1

--- Scripts/filter.R
+++ Scripts/filter.R
@@ -21,3 +21,44 @@ countries.filtered.48.summary.by.country <- group_by(countries.filtered.48, Coun
 
 countries.filtered.48.summary.by.series <- group_by(countries.filtered.48, Series) %>% 
   summarise(count=n())
+
+countries.filtered.48.edu <- filter(countries.filtered.48, Series.Code == "SE.PRM.TENR.FE" | Series.Code == "SE.PRM.TENR.MA"
+                                  | Series.Code == "SE.SEC.CMPT.LO.FE.ZS" | Series.Code == "SE.SEC.CMPT.LO.MA.ZS"
+                                  | Series.Code == "SE.PRM.NINT.FE.ZS" | Series.Code == "SE.PRM.NINT.MA.ZS"
+                                  | Series.Code == "SE.PRM.PRS5.FE.ZS" | Series.Code == "SE.PRM.PRS5.MA.ZS"
+                                  | Series.Code == "SE.PRM.CMPT.FE.ZS" | Series.Code == "SE.PRM.CMPT.MA.ZS"
+                                  | Series.Code == "SE.SEC.PROG.FE.ZS" | Series.Code == "SE.SEC.PROG.MA.ZS"
+                                  | Series.Code == "SE.PRM.CMPL.FE.ZS" | Series.Code == "SE.PRM.CMPL.MA.ZS"
+                                  | Series.Code == "SE.SCH.LIFE.FE" | Series.Code == "SE.SCH.LIFE.MA")
+
+countries.filtered.48.edu.summary.by.country <- group_by(countries.filtered.48.edu, Country.Name) %>% 
+  summarise(count=n())
+
+countries.filtered.48.edu.morocco <- filter(countries.filtered.48.edu, Country.Name == "Morocco")
+
+countries.filtered.48.edu.employ1 <- filter(countries.filtered.48, Series.Code == "SE.PRM.TENR.FE" | Series.Code == "SE.PRM.TENR.MA"
+                                    | Series.Code == "SE.SEC.CMPT.LO.FE.ZS" | Series.Code == "SE.SEC.CMPT.LO.MA.ZS"
+                                    | Series.Code == "SE.PRM.NINT.FE.ZS" | Series.Code == "SE.PRM.NINT.MA.ZS"
+                                    | Series.Code == "SE.PRM.PRS5.FE.ZS" | Series.Code == "SE.PRM.PRS5.MA.ZS"
+                                    | Series.Code == "SE.PRM.CMPT.FE.ZS" | Series.Code == "SE.PRM.CMPT.MA.ZS"
+                                    | Series.Code == "SE.SEC.PROG.FE.ZS" | Series.Code == "SE.SEC.PROG.MA.ZS"
+                                    | Series.Code == "SE.PRM.CMPL.FE.ZS" | Series.Code == "SE.PRM.CMPL.MA.ZS"
+                                    | Series.Code == "SE.SCH.LIFE.FE" | Series.Code == "SE.SCH.LIFE.MA"
+                                    
+                                    | Series.Code == "SL.EMP.SELF.FE.ZS" | Series.Code == "SL.EMP.SELF.MA.ZS"
+                                    | Series.Code == "IC.REG.COST.PC.FE.ZS" | Series.Code == "IC.REG.COST.PC.MA.ZS"
+                                    | Series.Code == "IC.REG.PROC.FE" | Series.Code == "IC.REG.PROC.MA"
+                                    | Series.Code == "IC.REG.DURS.FE" | Series.Code == "IC.REG.DURS.MA"
+                                    )
+
+countries.filtered.48.edu.employ1.by.country <- group_by(countries.filtered.48.edu.employ1, Country.Name) %>% 
+  summarise(count=n())
+
+countries.filtered.48.employ1 <- filter(countries.filtered.48,  Series.Code == "SL.EMP.SELF.FE.ZS" | Series.Code == "SL.EMP.SELF.MA.ZS"
+                                            | Series.Code == "IC.REG.COST.PC.FE.ZS" | Series.Code == "IC.REG.COST.PC.MA.ZS"
+                                            | Series.Code == "IC.REG.PROC.FE" | Series.Code == "IC.REG.PROC.MA"
+                                            | Series.Code == "IC.REG.DURS.FE" | Series.Code == "IC.REG.DURS.MA"
+)
+
+countries.filtered.48.employ1.by.country <- group_by(countries.filtered.48.employ1, Country.Name) %>% 
+  summarise(count=n())
